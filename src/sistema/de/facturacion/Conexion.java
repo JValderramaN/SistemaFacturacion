1fbb562744b89b16bebcdaf090c0fdc5527fa717
Luis <Luis@192.168.0.105>
2015-07-08 00:11:16 +0200

    primer coomit

---
+++ src/sistema/de/facturacion/Conexion.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sistema.de.facturacion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Karo
+ */
+public class Conexion {
+ 
+    static String bd = "ventas";   
+    static String login = "postgres";   
+    static String password = "123456";   
+    static String url = "jdbc:postgresql://localhost:5432/" ;   
+    static private Connection conn = null; 
+     
+    
+    
+    static public void createConexion() {      
+        try{        
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url+bd,login,password);
+            System.out.println("Conexion correcta "+conn);
+        }catch(SQLException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+             System.out.println(e);
+        }
+    }
+     
+   static  public Connection getConnection(){
+       if ( conn == null){
+           createConexion();
+       }
+       
+        return conn;
+    }
+     
+   static public void desconectar(){
+        conn = null;
+        System.out.println("La conexion a la  base de datos "+bd+" a terminado");
+    }
+}
